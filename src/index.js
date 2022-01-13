8347520d838f64a7eae40986a6efe2142388a4fa
Flyduckbb <76811484+Flyduckbb@users.noreply.github.com>
2022-01-13 22:32:11 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+import "./styles.css";
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+const title = document.querySelector("h2");
+
+const superEventHandler = {
+  handleMouseEnter() {
+    title.innerText = "The mouse is here!";
+    title.style.color = colors[0];
+  },
+
+  handleMouseLeave() {
+    title.innerText = "The mouse is gone!";
+    title.style.color = colors[1];
+  },
+
+  handleMouseRightClick() {
+    title.innerText = "That was a right click!";
+    title.style.color = colors[4];
+  },
+
+  handleWindowResize() {
+    title.innerText = "You just resized!";
+    title.style.color = colors[2];
+  }
+};
+
+title.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
+title.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
+window.addEventListener("contextmenu", superEventHandler.handleMouseRightClick);
+window.addEventListener("resize", superEventHandler.handleWindowResize);
